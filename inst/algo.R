f53a8a90b3341c9ca86c0fd0c6f96b2df6c35bad
LMDN <lmdn@novonordisk.com>
2022-05-03 16:11:52 +0200

    minor edits

--- inst/algo.R
+++ inst/algo.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(httr)
+devtools::load_all()
 
 # 24 hour token for simulation account
 # https://www.developer.saxo/openapi/token/current#/lst/1650285935777
-token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNzkzNTM2OGEzMzc0NGYxMjllYTE4NzUxZDFhZWMyNzgiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTA4NzM1ODEiLCJvYWwiOiIxRiJ9.B1TSVo6Lq4YznklMfVZXzcClmA0mUaqDXeKjJlxUH10eQuTtM_usNdGTiO0GYewa7krLXRN0HitvA6vln-xh4A"
+token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNTY2Zjk3NzUxNGExNGIwNjhlMTc5M2ZlOTU3MGVhZTQiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTE2NjMwMzUiLCJvYWwiOiIxRiJ9.FCuqIdSKC2f24tGluWr_GywcSmVf9SrkO8Le5HEuIz1HXjxcwKo2HhKZKuWrI2TeXgMErKYDyr-Czg4aGSVeNQ"
 token24 <- paste("Bearer", token24)
 
 cse <- get_cse_stocks(token24)
-uics <- paste(cse$Identifier, collapse = ",")
+uics <- paste(cse$Data.Identifier, collapse = ",")
 prc <- get_info_prices(token = token24, uics = uics)
 detail <- get_info_detail(token = token24, uics = uics)
--- inst/snippets.R
+++ inst/snippets.R
@@ -3,12 +3,14 @@ library(httr)
 
 # 24 hour token for simulation account
 # https://www.developer.saxo/openapi/token/current#/lst/1650285935777
-token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNzkzNTM2OGEzMzc0NGYxMjllYTE4NzUxZDFhZWMyNzgiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTA4NzM1ODEiLCJvYWwiOiIxRiJ9.B1TSVo6Lq4YznklMfVZXzcClmA0mUaqDXeKjJlxUH10eQuTtM_usNdGTiO0GYewa7krLXRN0HitvA6vln-xh4A"
+token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNTY2Zjk3NzUxNGExNGIwNjhlMTc5M2ZlOTU3MGVhZTQiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTE2NjMwMzUiLCJvYWwiOiIxRiJ9.FCuqIdSKC2f24tGluWr_GywcSmVf9SrkO8Le5HEuIz1HXjxcwKo2HhKZKuWrI2TeXgMErKYDyr-Czg4aGSVeNQ"
 token24 <- paste("Bearer", token24)
 
 # Basic request for balance
 r <- GET("https://gateway.saxobank.com/sim/openapi/port/v1/balances/me",
          add_headers(Authorization = token24))
+
+content(r)$CashBalance
 
 # available features
 r <- GET("https://gateway.saxobank.com/sim/openapi/root/v1/features/availability",
