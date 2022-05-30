34d74e283ed8f3d5caba0a5ed7e3b3590aaaa0c5
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2022-05-30 22:06:23 +0200

    option_prices.R script and get_schedule() function

--- R/wrappers.R
+++ R/wrappers.R
@@ -15,7 +15,7 @@ response_df <- function(response, remove_constant_cols = TRUE) {
     jsonlite::fromJSON(flatten = T) %>%
     as.data.frame() %>%
     dplyr::as_tibble() %>%
-    dplyr::mutate(dplyr::across(dplyr::ends_with("LastUpdated"), lubridate::as_datetime))
+    dplyr::mutate(dplyr::across(dplyr::ends_with(c("LastUpdated", "Time")), lubridate::as_datetime))
 
   if (remove_constant_cols & nrow(df) > 1) {
     df <- df %>% dplyr::select(where(~ dplyr::n_distinct(.) > 1))
@@ -53,11 +53,12 @@ get_exchanges <- function(token) {
 #' @param token character
 #' @param exchange_id character
 #' @param asset_type character
+#' @param ... parameters passed on to response_df()
 #'
 #' @return tibble
 #' @export
 #'
-get_assets <- function(token, exchange_id = "CSE", asset_type = "Stock", ...) {
+get_instruments <- function(token, exchange_id = "CSE", asset_type = "Stock", ...) {
   r <- httr::GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments",
       query = list(ExchangeId = exchange_id,
                    AssetTypes = asset_type,
@@ -80,6 +81,7 @@ get_assets <- function(token, exchange_id = "CSE", asset_type = "Stock", ...) {
 #' @param uics character, comma separated list of identifiers
 #' @param asset_type character, defaults to "stock"
 #' @param amount numeric, defaults to 10000
+#' @param ... parameters passed on to response_df()
 #'
 #' @return
 #' tibble with instruments (e.g. stocks)
@@ -104,19 +106,45 @@ get_info_prices <- function(token, uics, asset_type = "Stock", amount = 10000, .
 #' possibly a 24 hour token
 #' @param uics character, comma separated list of identifiers
 #' @param asset_type character, defaults to "stock"
+#' @param ... parameters passed on to response_df()
 #'
 #' @return
 #' tibble with instruments (e.g. stocks)
 #' @export
 #'
-get_info_detail <- function(token, uics, asset_type = "Stock") {
+get_details <- function(token, uics, asset_type = "Stock", ...) {
   r <- httr::GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/details",
       query = list(Uics = uics,
                    AssetType = asset_type),
       httr::add_headers(Authorization = token)
     )
 
-  info <- response_df(r)
+  info <- response_df(r, ...)
   info
 }
 
+
+#' Get trading schedule for a given uic and asset type
+#'
+#' @param token character, authorization token, for demo environments
+#' possibly a 24 hour token
+#' @param uic character, instrument identifier
+#' @param asset_type character, type of asset
+#' @param ... parameters passed on to response_df()
+#'
+#' @return tibble with trading schedule
+#' @export
+#'
+get_schedule <- function(token, uic, asset_type = "Stock", ...) {
+  url <- paste("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/tradingschedule",
+               uic, asset_type, sep = "/")
+
+  r <- httr::GET(url, query = list(Uic = uic, AssetType = asset_type),
+                 httr::add_headers(Authorization = token)
+  )
+
+  httr::stop_for_status(r)
+
+  schedule <- response_df(r, ...)
+  schedule
+}
--- inst/option_prices.R
+++ inst/option_prices.R
@@ -4,45 +4,55 @@
 library(tidyverse)
 library(httr)
 devtools::load_all()
-theme_set(theme_minimal())
 
 # 24 hour token for simulation account
 # https://www.developer.saxo/openapi/token/current#/lst/1650285935777
-token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZWY4MWZmNWEyMDcwNDk2YjkxZjBiZjRhNWVhZDA4MGEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTM1NzA0NDEiLCJvYWwiOiIxRiJ9.hwllV12gTz57U4izCkOOGy75KvhoQeO8XubkGCzNdSWQ8Uc_vo6RGMKZLZOy2eoY2T7gMPDR3a92O6qRvN1j_g"
+token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiYTRlNjhiNWE4ODEyNDk1MWJjY2Y0YTUxOTUxNmVhMjciLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTQwMjE5NjMiLCJvYWwiOiIxRiJ9.shHyod9hRMT4zS0MNeCj8Qa8YpMmPlWIDQuUQVp9nHZ6EMTUjBh_UwdjFPpGFfkEhEu29Q5sOkwcN5SVOFlJpA"
 token24 <- paste("Bearer", token24)
 
 # EUREX: Largest European futures and options market
 # OPRA: Options Price Reporting Authority for US
-opt <- get_assets(token24, exchange_id = "EUREX", asset_type = "StockOption", remove_constant_cols = F)
 
-uics <- opt$Data.Identifier %>% head(5) %>% paste0(collapse = ",")
+# Ref:
+# https://www.developer.saxo/openapi/learn/reference-data?phrase=contractoptionspaces
 
-prc <- get_info_prices(token24, uics = uics, asset_type = "StockOption")
+# 1. Get list of options via instruments end point
+opt <- get_instruments(token24, exchange_id = "EUREX", asset_type = "StockOption", remove_constant_cols = F)
 
+# 2. Identifier field will contain Option Root ID (collection of related options)
+root_id <- opt %>% filter(str_detect(Data.Description, "Heidelberger Cement AG")) %>% pull(Data.Identifier)
 
-# try this
-# 1. Get list of options like in opt above
-# 2. Get uics from those
-# 3. Get optionRootIds for those, as explained here:
-# https://openapi.help.saxo/hc/en-us/articles/4417056831633-How-do-I-find-the-OptionRootId-for-an-option-
-# 4. Then, get option root info including price(?) like below:
-
-# Detailed information about option root (optionRootId = 7) :
-r2 <- GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/contractoptionspaces/7",
-         query = list(ClientKey = "mM3WZ5aMVM|2gm5fOyrLkw=="), # How to pass OptionRouteId to call? Not like this
+# 3. Use root id to get option space from contractoptionspaces end point. Try option root = 572 :
+# Option space can be huge, so limit using OptionSpaceSegment parameter (how does that work, exactly?)
+r <- GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/contractoptionspaces/572",
+         query = list(ClientKey = "mM3WZ5aMVM|2gm5fOyrLkw==",
+                      OptionSpaceSegment = "SpecificDates", #?
+                      ExpiryDates = "2023-08-16", #?
+                      UnderlyingUic = 15611 #?
+         ),
          add_headers(Authorization = token24)
 )
 
-http_status(r2)
-content(r2)
-response_df(r2)
+http_status(r)
 
-df <- r2 %>%
-  httr::content(as = "text") %>%
-  jsonlite::fromJSON(flatten = T) %>%
-  as.data.frame() %>% # Breaks here, complex response at this end point
-  dplyr::as_tibble() %>%
-  dplyr::mutate(dplyr::across(dplyr::ends_with("LastUpdated"), lubridate::as_datetime))
+# 4. Examine response to find the option we need.
+specific_options <- r %>%
+  content() %>%
+  pluck("OptionSpace") %>%
+  pluck(7) %>%
+  pluck("SpecificOptions") %>%
+  bind_rows() %>%
+  filter(PutCall == "Call", TradingStatus == "Tradable")
 
-df$RelatedInstruments
+uics <- paste(specific_options$Uic, collapse = ",")
 
+prices <- get_info_prices(token = token24, uics = uics, asset_type = "StockOption")
+
+glimpse(prices)
+
+sch <- get_schedule(token = token24, uic = "18094550", asset_type = "StockOption")
+
+# To do:
+# Understand option space
+# Understand OptionSpaceSegment parameter
+# Try optionchain endpoint
