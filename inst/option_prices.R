1e2ae0c48e2feb5185b1a6208a1c3e7c4555fcbb
LMDN <lmdn@novonordisk.com>
2022-06-17 17:26:15 +0200

    trying streaming prices

--- inst/option_prices.R
+++ inst/option_prices.R
@@ -1,14 +1,19 @@
-# A go at fetching option prices -- after
-# connecting dev account to live account
+# Snippets for getting option prices
 
-library(tidyverse)
 library(httr)
+library(tidyverse)
+library(jsonlite)
 devtools::load_all()
 
-# 24 hour token for simulation account
-# https://www.developer.saxo/openapi/token/current#/lst/1650285935777
-token24 <- "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiODY2YTJkZjYyNDdjNDc5MGE4YTIzZjZkOWRhNWQ4ZGEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NTQxMTQ0MDgiLCJvYWwiOiIxRiJ9.L-PD61_5wqqi6tR1mTF0X7htFF008OPYpXsKpuTt9IMSHOGfYg5e1Zf3t1NTHe7nw69CGido52zhvNaojZIJVQ"
-token24 <- paste("Bearer", token24)
+# Token for live environment
+my_token <- authorize_live()
+
+# My info
+my_info <- get_client_info(token = my_token, live = TRUE)
+
+# My default key
+account_key <- my_info$DefaultAccountKey[1]
+client_key <- my_info$ClientKey[1]
 
 # EUREX: Largest European futures and options market
 # OPRA: Options Price Reporting Authority for US
@@ -17,22 +22,23 @@ token24 <- paste("Bearer", token24)
 # https://www.developer.saxo/openapi/learn/reference-data?phrase=contractoptionspaces
 
 # 1. Get list of options via instruments end point
-opt <- get_instruments(token24, exchange_id = "EUREX", asset_type = "StockOption", remove_constant_cols = F)
+opt <- get_instruments(token = my_token, live = TRUE, exchange_id = "EUREX", asset_type = "StockOption")
 
-# 2. Identifier field will contain Option Root ID (collection of related options)
+# 2. Identifier field will contain Option Root ID (collection of related options). E.g.:
 root_id <- opt %>% filter(str_detect(Data.Description, "Heidelberger Cement AG")) %>% pull(Data.Identifier)
 
 # 3. Use root id to get option space from contractoptionspaces end point. Try option root = 572 :
 # Option space can be huge, so limit using OptionSpaceSegment parameter (how does that work, exactly?)
-r <- GET("https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/contractoptionspaces/572",
-         query = list(ClientKey = "mM3WZ5aMVM|2gm5fOyrLkw==",
+r <- httr::GET("https://gateway.saxobank.com/openapi/ref/v1/instruments/contractoptionspaces/572",
+         query = list(ClientKey = client_key,
                       OptionSpaceSegment = "DefaultDates",
                       UnderlyingUic = "1"
-         ),
-         add_headers(Authorization = token24)
-)
+                      ),
+         config = my_token
+         )
 
-http_status(r)
+
+httr::http_status(r)
 
 # 4. Examine response to find the option we need.
 specific_options <- r %>%
@@ -42,12 +48,69 @@ specific_options <- r %>%
   bind_rows() %>%
   filter(PutCall == "Call", TradingStatus == "Tradable")
 
-uics <- paste(specific_options$Uic, collapse = ",")
+# 5. Set up price subscription
+context_id <- c(letters, LETTERS, 0:9, "-") %>%
+  sample(size = 10, replace = TRUE) %>%
+  paste(collapse = "")
 
-prices <- get_info_prices(token = token24, uics = uics, asset_type = "StockOption", remove_constant_cols = F)
+reference_id <- c(letters, LETTERS, 0:9, "-") %>%
+  sample(size = 10, replace = TRUE) %>%
+  paste(collapse = "")
 
-prices %>% glimpse()
-prices %>% view("prices")
+body <- list(
+  "Arguments" = list("Uic" = 16829451,
+                     "AssetType" = "StockOption"),
+  "ContextId" = "explorer_1655456847440",
+  "ReferenceId" = "K_241"
+)
 
-sch <- get_schedule(token = token24, uic = "18094550", asset_type = "StockOption", remove_constant_cols = F)
+r <- POST("https://gateway.saxobank.com/openapi/trade/v1/prices/subscriptions",
+          write_stream(function(x) {
+            print(length(x))
+            length(x)
+          }),
+          body = body,
+          config = my_token,
+          encode = "json"
+)
 
+http_status(r)
+
+content(r)
+response_df(r)
+
+
+
+
+
+# 6. Use optionschain endpoint
+
+
+body <- list(
+  "Arguments" = list("AssetType" = "StockOption",
+                     "Identifier" = root_id),
+  "ContextId" = context_id,
+  "ReferenceId" = reference_id
+)
+
+
+r <- POST("https://gateway.saxobank.com/openapi/trade/v1/optionschain/subscriptions",
+          body = jsonlite::toJSON(body),
+          config = my_token,
+          encode = "form"
+          )
+
+r <- POST("https://gateway.saxobank.com/openapi/trade/v1/optionschain/subscriptions",
+          body = jsonlite::toJSON(body),
+          config = my_token,
+          encode = "json"
+)
+
+r <- POST("https://gateway.saxobank.com/openapi/trade/v1/optionschain/subscriptions",
+          body = body,
+          config = my_token,
+          encode = "form"
+)
+
+
+http_status(r)
